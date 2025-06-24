97c546981015794d45b2a8db9f3eb6ea79c375fc
santioki <mcskilled7@gmail.com>
2025-06-24 22:25:41 +0200

    Fix booking POST request URL to use relative path

--- client/script.js
+++ client/script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const response = await fetch("https://www.prowash.it.com/api/bookings", {
+      const response = await fetch("/api/bookings", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
